117936d3afa1db838aaef87dcbb288c5e26b7af2
Sylvain Pluchart <sylvain.pluchart@ec-lyon.fr>
2024-12-18 11:21:10 +0100

    [vim.opt] added settings for tab expansion to  4 spaces

--- lua/kurwa_config/opt.lua
+++ lua/kurwa_config/opt.lua
@@ -5,3 +5,8 @@ vim.opt.mouse = 'a'
 
 -- Sync clipboard between neovim and OS
 vim.opt.clipboard = 'unnamedplus'
+
+-- Tab vs. space
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true	    
