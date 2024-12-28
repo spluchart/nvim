beed0abf6b27bf83cb1de42b1aaccf4afec335f6
Sylvain Pluchart <sylvain.pluchart@ec-lyon.fr>
2024-12-28 14:29:32 +0100

    [VimTeX] added a viewer for Windows machines (SumatraPDF)

--- after/plugin/vimtex.lua
+++ after/plugin/vimtex.lua
@@ -1,7 +1,12 @@
 vim.g.maplocalleader = ","
 
 -- viewer option
-vim.g.vimtex_view_method = 'zathura'
+if vim.fn.has('macunix') == 0 then  -- Windows
+    vim.g.vimtex_view_general_viewer = 'SumatraPDF'
+    vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
+else  -- Linux
+    vim.g.vimtex_view_method = 'zathura'
+end
 
 -- VimTeX use latexmk as the default compiler backend.
 -- Uncomment if you want to use a different one.
